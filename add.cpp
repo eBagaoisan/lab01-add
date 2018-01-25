59cc5f48a477a7b716a0633bd9e3cf6639c72764
ebagaoisan <ebagaoisan1@mail.csuchico.edu>
2018-01-25 09:36:28 -0800

    fixed cout

--- add.cpp
+++ add.cpp
@@ -7,9 +7,8 @@ int main(){
     int num1;
     int num2;
     int sum;
-    cout<<"Please enter two numbers: ";
     cin>>num1>>num2;
     sum = num1 + num2;
-    cout<<"Please enter two numbers: "<<num1<<" + "<<num2<<" = "<<sum<<endl;
+    cout<<num1<<" + "<<num2<<" = "<<sum<<endl;
     return 0;
 }
