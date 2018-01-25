73ce4bc2889dfe9a7ba7044f02f89946a7fcb869
ebagaoisan <ebagaoisan1@mail.csuchico.edu>
2018-01-25 09:33:11 -0800

    Fixed text

--- add.cpp
+++ add.cpp
@@ -8,9 +8,8 @@ int main(){
     int num2;
     int sum;
     cout<<"Please enter two numbers: ";
-    cin>>num1;
-    cin>>num2;
+    cin>>num1>>num2;
     sum = num1 + num2;
-    cout<<num1<<" + "<<num2<<" = "<<sum<<endl;
+    cout<<"Please enter two numbers: "<<num1<<" + "<<num2<<" = "<<sum<<endl;
     return 0;
 }
